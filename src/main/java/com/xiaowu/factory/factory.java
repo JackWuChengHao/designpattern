7c39639dfcef11a58169bbb299665cbb5d88165a
xiaowu <2457893105@qq.com>
2018-02-25 16:08:04 +0800

    修改内容

--- src/main/java/com/xiaowu/factory/factory.java
+++ src/main/java/com/xiaowu/factory/factory.java
@@ -2,8 +2,6 @@ package com.xiaowu.factory;
 
 public class factory {
 	public product getproduct(Class<?> clas) throws InstantiationException, IllegalAccessException{
-ss
 		return (product)clas.newInstance();
-
 	}
 }
